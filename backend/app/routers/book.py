7b012575e18aa7d154b5adf0e5c292cbb9732af8
huytht <huytht2001@gmail.com>
2021-12-08 16:27:16 +0700

    update function search for book

--- backend/app/routers/book.py
+++ backend/app/routers/book.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+from typing import Optional
 from fastapi import APIRouter, Body, Request, HTTPException, status
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from ..models.book import BookModel, BookUpdateModel
 from ..routers.category import list_category
+import re
 
 router = APIRouter()
 
@@ -37,17 +39,24 @@ async def list_books(request: Request):
                         break
 
             book['category'] = category_name
-
             books.append(book)
 
     return books
 
-
 @router.get("/list-book-by-category/{id}", response_description="find list book by category")
 async def find_list_book_by_category(id: int, request: Request):
     books = []
-    for book in await request.app.mongodb["book"].find({'category_id': id}).to_list(length=100):
-        books.append(book)
+    if id == 1:
+        for book in await request.app.mongodb["book"].find().to_list(length=500):
+            if isinstance(book["_id"], int):
+                book['rate'] = await get_rate_book(book['_id'], request)
+                book['review'] = len(await get_review_book(book['_id'], request))
+                books.append(book)
+    else:
+        for book in await request.app.mongodb["book"].find({'category_id': id}).to_list(length=100):
+            book['rate'] = await get_rate_book(book['_id'], request)
+            book['review'] = len(await get_review_book(book['_id'], request))
+            books.append(book)
 
     return books
 
@@ -55,7 +64,29 @@ async def find_list_book_by_category(id: int, request: Request):
 @router.get("/list-book-by-name/{keyword}", response_description="find list book by name")
 async def find_list_book_by_name(keyword: str, request: Request):
     books = []
-    for book in await request.app.mongodb["book"].find({"title": {"$regex": keyword}}).to_list(length=100):
+    for book in await request.app.mongodb["book"].find({"title": {"$in": [re.compile(f'{keyword.lower()}'), re.compile(f'{keyword.capitalize()}')]}}).to_list(length=100):
+        book['rate'] = await get_rate_book(book['_id'], request)
+        book['review'] = len(await get_review_book(book['_id'], request))
+        books.append(book)
+
+    return books
+
+@router.get("/list-book-by-author/{keyword}", response_description="find list book by name")
+async def find_list_book_by_author(keyword: str, request: Request):
+    books = []
+    for book in await request.app.mongodb["book"].find({"author": keyword}).to_list(length=100):
+        book['rate'] = await get_rate_book(book['_id'], request)
+        book['review'] = len(await get_review_book(book['_id'], request))
+        books.append(book)
+
+    return books
+
+@router.get("/list-book-by-publisher/{id}", response_description="find list book by name")
+async def find_list_book_by_publisher(id: int, request: Request):
+    books = []
+    for book in await request.app.mongodb["book"].find({"publisher_id": id}).to_list(length=100):
+        book['rate'] = await get_rate_book(book['_id'], request)
+        book['review'] = len(await get_review_book(book['_id'], request))
         books.append(book)
 
     return books
@@ -65,10 +96,10 @@ async def find_list_book_by_name(keyword: str, request: Request):
 async def list_best_books(request: Request):
     books = []
 
-    for book in await request.app.mongodb["book"].find().to_list(length=8):
+    for book in await request.app.mongodb["book"].find().to_list(length=9):
         if isinstance(book["_id"], int):
             book['rate'] = await get_rate_book(book['_id'], request)
-            book['review'] = await get_review_book(book['_id'], request)
+            book['review'] = len(await get_review_book(book['_id'], request))
             books.append(book)
 
     return books
@@ -81,7 +112,7 @@ async def list_newest_books(request: Request):
     for book in await request.app.mongodb["book"].find(sort=[('$natural', -1)]).to_list(length=8):
         if isinstance(book["_id"], int):
             book['rate'] = await get_rate_book(book['_id'], request)
-            book['review'] = await get_review_book(book['_id'], request)
+            book['review'] = len(await get_review_book(book['_id'], request))
             books.append(book)
 
     return books
@@ -97,6 +128,7 @@ async def get_book(id: int, request: Request):
         if (publisher := await request.app.mongodb["publisher"].find_one({"_id": book['publisher_id']})) is not None:
             book['publisher'] = publisher
         book['category'] = category_name
+        book['related_book'] = await find_list_book_related(book['category_id'][0], request)
         book['quantity_active'] = await get_quantity_active_book(book['_id'], request)
         book['rate'] = await get_rate_book(book['_id'], request)
         book['review'] = await get_review_book(book['_id'], request)
@@ -105,6 +137,15 @@ async def get_book(id: int, request: Request):
 
     raise HTTPException(status_code=404, detail="Book {id} not found")
 
+@router.get("/list-book-related/{id}", response_description="find list book by name")
+async def find_list_book_related(id: int, request: Request):
+    books = []
+    for book in await request.app.mongodb["book"].find({"category_id": id}).to_list(length=8):
+        book['rate'] = await get_rate_book(book['_id'], request)
+        book['review'] = len(await get_review_book(book['_id'], request))
+        books.append(book)
+
+    return books
 
 @router.get("/get-quantity-active-book/{id}", response_description="Get quantity active book")
 async def get_quantity_active_book(id: int, request: Request):
